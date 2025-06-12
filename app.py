6dcce827058775cf880e0707b1a028641cfc3448
Lazybear-cloud <wolfht93@gmail.com>
2025-06-12 13:57:53 +0900

    Update app.py

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ latest_date = vix_data.index[-1].date()
 # ✅ 2컬럼 레이아웃 구성
 col1, col2 = st.columns([2, 1])  # 왼쪽(col1)이 더 넓게
 
-with col1
+with col1:
     # ✅ Plotly 그래프 생성
     fig = go.Figure()
     
