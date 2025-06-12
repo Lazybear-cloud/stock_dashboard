ba3f55674f06d2cf34f96631c6859e22c73a8f15
Lazybear-cloud <wolfht93@gmail.com>
2025-06-12 13:41:27 +0900

    Update app.py

--- app.py
+++ app.py
@@ -23,21 +23,6 @@ st.line_chart(vix_data["Close"])
 
 
 
-one_year_ago = pd.Timestamp.today() - pd.Timedelta(days=365)
-vix_last_year = vix_data[vix_data.index >= one_year_ago]
-
-
-# 날짜 범위 선택 슬라이더
-start_date = st.date_input("시작 날짜", one_year_ago.date())
-end_date = st.date_input("종료 날짜", vix_data.index[-1].date())
-
-# 선택된 범위로 필터링
-filtered_data = vix_data.loc[start_date:end_date]
-
-# 그래프 출력
-st.line_chart(filtered_data["Close"])
-
-
 
 # 평균값 계산
 mean_value = vix_data["Close"].mean()
